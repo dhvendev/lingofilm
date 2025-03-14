1d91c745dbfedf215dc764265c8a24b64195b9a8
dhvendev <dhvendev@gmail.com>
2025-03-14 14:56:18 +0300

    Add theme chenge toogle, edit login form + add zod validate for email

--- components/users/LoginForm.jsx
+++ components/users/LoginForm.jsx
@@ -2,6 +2,26 @@
 import { useState } from "react";
 import { loginUser } from "@/services/axiosMethods";
 import { useUser } from "@/context/userContext";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,    
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+    } from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { z } from "zod";
+
+const loginSchema = z.object({
+    email: z.string().email("Введите корректный адрес электронной почты").min(1, "Email обязателен"),
+    password: z.string().min(4, "Пароль должен быть минимум из 6 символов").min(1, "Пароль обязателен")
+});
 
 export default function LoginForm() {
     const { setUser } = useUser();
@@ -14,6 +34,8 @@ export default function LoginForm() {
         setError(null);
 
         try {
+            loginSchema.parse({ email, password });
+
             const response = await loginUser(email, password);
             if (!response) {
                 setError("Ошибка входа. Проверьте логин и пароль.");
@@ -21,20 +43,39 @@ export default function LoginForm() {
                 setUser(response);
             }
         } catch (err) {
-            setError("Ошибка сервера. Попробуйте позже.");
+            if (err instanceof z.ZodError) {
+                setError(err.errors.map(e => e.message).join(", ")); // Сообщаем о ошибках валидации
+            } else {
+                setError("Ошибка сервера. Попробуйте позже.");
+            }
             console.error("Ошибка при входе:", err);
         }
     };
-
     return (
-        <form onSubmit={handleSubmit} method="GET" className="flex flex-col gap-4">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" required onChange={(e) => setEmail(e.target.value)} className="bg-black"/>
-            
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" required onChange={(e) => setPassword(e.target.value)} className="bg-black" />
-            
-            <button type="submit">Login</button>
-        </form>
-    );
+        <>
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                <Button className="primary1 rounded ml-2">Войти в аккаунт</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Вход</AlertDialogTitle>
+                    {error && <p className="text-red-500">{error}</p>}
+                    <AlertDialogDescription className="flex flex-col gap-3 my-2">
+                            <Label htmlFor="email">Электронная почта:</Label>
+                            <Input type="email" name="email" id="email" placeholder="welcome@lingofilms.ru" required onChange={(e) => setEmail(e.target.value)} className="bg-black"/>
+                            <Label htmlFor="password">Пароль:</Label>
+                            <Input type="password" name="password" id="password" placeholder="lingo_secret" required onChange={(e) => setPassword(e.target.value)} className="bg-black" />
+
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={handleSubmit}>Continue</AlertDialogAction>
+                </AlertDialogFooter>    
+            </AlertDialogContent>
+        </AlertDialog>
+        </>
+
+    )
 }
